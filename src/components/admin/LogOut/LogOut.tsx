724ae767b2482f6156aafcd0caf5423abb9805b3
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2024-04-06 18:31:33 +0200

    fix inconsistency in file names

--- src/components/admin/LogOut/LogOut.tsx
+++ src/components/admin/LogOut/LogOut.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
+import React from "react";
 import Image from "next/image";
-import logOut from "./logOut.svg"
-import {cookies} from "next/headers";
+import logOut from "./logOut.svg";
+import { cookies } from "next/headers";
 
 const LogOut = () => {
+  async function createInvoice() {
+    "use server";
+    cookies().delete("logIn");
+    cookies().delete("id");
+    cookies().delete("email");
+    cookies().delete("role");
+    cookies().delete("token");
+  }
 
-    async function createInvoice() {
-        'use server'
-        cookies().delete('logIn')
-        cookies().delete('id')
-        cookies().delete('email')
-        cookies().delete('role')
-        cookies().delete('token')
-    }
-
-    return (
-        <form action={createInvoice}>
-            <button>
-                <Image src={logOut} alt={"logo"}/>
-            </button>
-        </form>
-    );
+  return (
+    <form action={createInvoice}>
+      <button title="logout">
+        <Image src={logOut} alt={"logo"} />
+      </button>
+    </form>
+  );
 };
 
 export default LogOut;
