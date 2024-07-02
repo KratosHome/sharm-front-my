02e0debde5be5d4f7d21c2e592e9270ba598105e
andrii-pozniak <andriipozniak@gmail.com>
2024-07-02 20:12:16 +0300

    add page forgotPassword

--- src/app/[locale]/(client)/(auth)/forgot-password/page.tsx
+++ src/app/[locale]/(client)/(auth)/forgot-password/page.tsx
@@ -1,6 +1,9 @@
 import {auth} from "@/server/auth/auth";
 import {redirect} from "next/navigation";
 import React from "react";
+import '../login/login.scss'
+import ForgotPassword from "@/components/client/auth/ForgotPassword/ForgotPassword";
+import { Footer } from "@/components/client/Footer/Footer";
 
 export default async function Page({params: {locale}}: any) {
     const session = await auth();
@@ -9,7 +12,9 @@ export default async function Page({params: {locale}}: any) {
       //  redirect(`/${locale}/profile`);
     }
     return (
-        <>
-        </>
+      <div className="loginWrapper ">
+        <ForgotPassword/>
+        <Footer/>
+        </div>
     );
 }
